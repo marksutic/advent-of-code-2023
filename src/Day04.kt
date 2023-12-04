feb4a19e861a8d04206db73c0028208f863407c6
Marko Šutić <mark.sutic@gmail.com>
2023-12-04 17:00:52 +0100

    day 4 star 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -11,26 +11,46 @@ fun main() {
   }
 
   fun part2(input: List<String>): Int {
-    return 1
+    val cards = input.map { it.toCard() }
+    val cardsMap = mutableMapOf<Int, Int>()
+
+    cards.forEachIndexed { index, _ -> cardsMap[index + 1] = 1 }
+    cardsMap.printlnDebug()
+
+    cards.forEachIndexed { index, card ->
+      "index: $index".printlnDebug()
+      val winningNumbers = card.winningNumbers.intersect(card.numbers.toSet()).size
+      "winningNumbers: $winningNumbers".printlnDebug()
+      val cardsCount = cardsMap[index + 1]
+      "cardsCount: $cardsCount".printlnDebug()
+      for (winCardNumber in (index + 2)..(index + 1 + winningNumbers)) {
+        "winCardNumber: $winCardNumber".printlnDebug()
+        cardsMap[winCardNumber] = cardsMap[winCardNumber]!! + cardsCount!!
+      }
+    }
+    cardsMap.printlnDebug()
+    return cardsMap.values.sum()
   }
 
-  val testInputFirstStar = readInput("Day04_1_test")
+  val testInputFirstStar = readInput("Day04_test")
   check(part1(testInputFirstStar) == 13)
 
   val input = readInput("Day04")
   println("first star: ${part1(input)}")
 
-//  val testInputSecondStar = readInput("Day04_2_test")
-//  check(part2(testInputSecondStar) == 1)
+  val testInputSecondStar = readInput("Day04_test")
+  DEBUG = true
+  check(part2(testInputSecondStar) == 30)
 
 //  println("second star: ${part2(input)}")
 }
 
 private data class Card(
-  val id:Int,
-  val winningNumbers:List<Int>,
-  val numbers:List<Int>,
+  val id: Int,
+  val winningNumbers: List<Int>,
+  val numbers: List<Int>,
 )
+
 private fun String.toCard(): Card = Card(
   id = this.substringAfter("Card").substringBefore(":").trim().toInt(),
   winningNumbers = this.substringAfter(":")
